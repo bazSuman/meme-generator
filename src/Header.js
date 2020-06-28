2f46e6da3e793d505cabc5e47616a30b7054e8a0
bazSuman <bazsuman07@gmail.com>
2020-06-28 23:48:58 +0545

    cfclogotrial

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import meme from './meme.jpg'
+import cfc from './cfc.png'
 import './App.css'
 
 function Header(){
@@ -7,6 +8,7 @@ function Header(){
         <header>
             <h1> [BazSuman] - Presents MemeGenerator </h1>
             <img id="header_image" src={meme} alt="Banner" style={{marginTop:"-1.2%", position:"relative", width:"100vw"}}/>
+            <img id="header_image2" src={cfc} alt="Banner" style={{marginTop:"-1.2%", position:"relative", width:"100vw"}}/>
         </header>
         )
 }
