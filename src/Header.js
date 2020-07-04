0bdb190f8d4c642b59e9a58c1f40c255711711fc
bazSuman <bazsuman07@gmail.com>
2020-07-04 16:00:05 +0545

    removed logo

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import meme from './meme.jpg'
-import cfc from './cfc.png'
 import './App.css'
 
 function Header(){
     return(
         <header>
-            <h1> [BazSuman] - Presents MemeGenerator </h1>
+            <h1 style={{paddingTop:'-12px%'}}> [BazSuman] - Presents MemeGenerator </h1>
             <img id="header_image" src={meme} alt="Banner" style={{marginTop:"-1.2%", position:"relative", width:"100vw"}}/>
-            {/* <img id="header_image2" src={cfc} alt="Banner" style={{marginTop:"-1.2%", position:"relative", width:"100vw"}}/> */}
         </header>
         )
 }
