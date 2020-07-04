4ffea7775e75a951da11272c43fd6fb51b2c407f
bazSuman <bazsuman07@gmail.com>
2020-07-04 15:52:34 +0545

    cfc logo changed

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ function Header(){
         <header>
             <h1> [BazSuman] - Presents MemeGenerator </h1>
             <img id="header_image" src={meme} alt="Banner" style={{marginTop:"-1.2%", position:"relative", width:"100vw"}}/>
-            <img id="header_image2" src={cfc} alt="Banner" style={{marginTop:"-1.2%", position:"relative", width:"100vw"}}/>
+            {/* <img id="header_image2" src={cfc} alt="Banner" style={{marginTop:"-1.2%", position:"relative", width:"100vw"}}/> */}
         </header>
         )
 }
